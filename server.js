d3aef03c05a96bfc73de4d9e53241c3f98ff4a88
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-08-03 13:27:36 +0530

    Add al files of server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const { dirname } = require('path')
+const { log } = require('console')
+const app = express()
+const data = fs.readFileSync('./Data/data.json',
+{encoding:'utf8', flag:'r'});
+app.use(cors())
+app.get('/v1/api/countries', (req,res)=>{
+res.json(data)
+} )
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, ()=>console.log(`Server is running on ${PORT}`))
